ee6ec793f086ede1565dac5e371861a69542d143
Jan De Poorter <jan@defv.be>
2025-12-04 21:28:51 +0100

    Day 4, part 2 — Quite the refactor

--- day04-printing-department/src/main.rs
+++ day04-printing-department/src/main.rs
@@ -1,10 +1,4 @@
-use std::collections::HashMap;
-
-fn main() {
-    let input = aoc::input();
-
-    println!("Step 1: {}", step1(&input));
-}
+use std::collections::HashSet;
 
 const ADJACENT_POSITIONS: [(isize, isize); 8] = [
     (-1, -1),
@@ -17,46 +11,65 @@ const ADJACENT_POSITIONS: [(isize, isize); 8] = [
     (1, 1),
 ];
 
-fn step1(input: &str) -> usize {
-    let mut paper_positions = Vec::new();
-    let mut adjacency_count: HashMap<(usize, usize), usize> = HashMap::new();
+const PAPER_CHAR: char = '@';
 
-    for (line, row) in input.lines().enumerate() {
-        for (col, tile) in row.chars().enumerate() {
-            let tile: Tile = tile.into();
+type Position = (usize, usize);
+struct Warehouse {
+    paper_positions: HashSet<Position>,
+}
 
-            if matches!(tile, Tile::Paper) {
-                paper_positions.push((col, line));
+impl Warehouse {
+    fn from_input(input: &str) -> Self {
+        let paper_positions = input
+            .lines()
+            .enumerate()
+            .flat_map(|(line, row)| {
+                row.chars().enumerate().filter_map(move |(col, tile)| {
+                    if tile == PAPER_CHAR {
+                        Some((col, line))
+                    } else {
+                        None
+                    }
+                })
+            })
+            .collect();
 
-                ADJACENT_POSITIONS
-                    .iter()
-                    .filter_map(|(x, y)| {
-                        Some((col.checked_add_signed(*x)?, line.checked_add_signed(*y)?))
-                    })
-                    .for_each(|(col, line)| {
-                        *adjacency_count.entry((col, line)).or_insert(0) += 1;
-                    });
+        Self { paper_positions }
+    }
+
+    fn reachable_positions(&self) -> HashSet<Position> {
+        self.paper_positions.clone().iter().filter_map(|(col, line)| {
+            let adjacent_positions = ADJACENT_POSITIONS.iter().filter_map(|(x, y)| {
+                Some((col.checked_add_signed(*x)?, line.checked_add_signed(*y)?))
+            }).collect::<HashSet<Position>>();
+
+            if self.paper_positions.intersection(&adjacent_positions).count() < 4 {
+                Some((*col, *line))
+            } else {
+                None
             }
-        }
+        }).collect()
     }
 
-    paper_positions
-        .into_iter()
-        .filter(|(col, line)| *adjacency_count.get(&(*col, *line)).unwrap_or(&0) < 4)
-        .count()
-}
-
-enum Tile {
-    Paper,
-    Empty,
-}
-
-impl From<char> for Tile {
-    fn from(c: char) -> Self {
-        match c {
-            '.' => Tile::Empty,
-            '@' => Tile::Paper,
-            _ => panic!("Invalid tile character: {}", c),
+    fn remove_reachable_positions(&self, reachable_positions: HashSet<Position>) -> Self {
+        Self {
+            paper_positions: self.paper_positions.difference(&reachable_positions).cloned().collect(),
         }
     }
+}
+
+fn main() {
+    let input = aoc::input();
+    let mut warehouse = Warehouse::from_input(&input);
+
+    println!("Step 1: {}", warehouse.reachable_positions().len());
+
+    let mut removed_positions = 0;
+
+    while let reachable_positions = warehouse.reachable_positions() && reachable_positions.len() > 0 {
+        removed_positions += reachable_positions.len();
+        warehouse = warehouse.remove_reachable_positions(reachable_positions);
+    }
+
+    println!("Step 2: {}", removed_positions);
 }
